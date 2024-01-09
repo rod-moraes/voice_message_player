2da7ada188bcc2fa6356f9907bfbe4e5156b2002
Rodrigo Carvalho Moraes <rodmoraes@Mac-Studio-de-Rodrigo.local>
2024-01-09 15:13:20 -0300

    fix rebase

--- lib/src/helpers/utils.dart
+++ lib/src/helpers/utils.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 /// Get screen media.
 final MediaQueryData media =
     // ignore: deprecated_member_use
-    MediaQueryData.fromView(WidgetsBinding.instance.window);
+    MediaQueryData.fromWindow(WidgetsBinding.instance.window);
 
 /// This extention help us to make widget responsive.
 extension NumberParsing on num {
